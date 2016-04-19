04b414add251a45b7fdbd3de55bf9c76ecbf9966
birdy911 <ryan.nair01@gmail.com>
2016-04-19 10:35:51 +0200

    Added files via upload
    added shell script file

---
+++ complete/runMvn.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+cd $(dirname $0)
+
+if [ ! -d "$ANDROID_HOME" ]; then
+    echo "ANDROID_HOME is not available"
+    exit
+fi
+
+if [ ! -d "$MAVEN_HOME" ]; then
+    echo "MAVEN_HOME is not available"
+    exit
+fi
+
+mvn clean install
+ret=$?
+
+if [ $ret -ne 0 ]; then
+    exit $ret
+fi
+rm -rf build
+
+exit
